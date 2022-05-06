f5ca48b7854a3d1fcf1ec7c413d2b7806e37814e
Marcelo Marchant <marcelo.marchant.g@gmail.com>
2022-05-06 14:47:52 -0400

    Se incorporan eventos (se crea el carro de compras provisorio)

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 
 // Variables
 let item = ""
-let cantidad = 0
+
 let detalle = ""
 let totalCarro = ""
 let totalMonto = 0
@@ -10,11 +10,12 @@ let totalMonto = 0
 let productos = []
 // Se define la clase para crear los objetos
 class Producto {
-    constructor(tipo, codigoVenta, nombre, precio, estado, descripcion, imagen) {
+    constructor(tipo, codigoVenta, nombre, precio, cantidad, estado, descripcion, imagen) {
         this.tipo = tipo.toUpperCase();
         this.codigoVenta = codigoVenta.toUpperCase();
         this.nombre = nombre;
         this.precio = parseInt(precio);
+        this.cantidad = 1;
         this.estado = "activo";
         this.descripcion = descripcion;
         this.imagen = imagen;
@@ -22,22 +23,22 @@ class Producto {
 }
 
 // Se crean los objetos
-productos.push(new Producto("h", "h1", "Rod Big Boss", 6500, "", "Jugosa hamburguesa doble con cheddar, tocino, aros de cebolla y un huevo frito.", "img/h-rod-big-boss.jpeg"))
-productos.push(new Producto("h", "h2", "Rod Dills", 5500, "", "Jugosa hamburguesa sobre cama de lechuga, con cheddar pepinillos y mayonesa casera, simplemente deliciosa.", "img/h-rod-dills.jpeg"))
-productos.push(new Producto("h", "h3", "Rod Spicy", 6600, "", "Para los amantes del picante, deliciosa hamburguesa con cheddar, jalapeño, cebolla morada en cuadritos, papas hilo y la salsa secreta de la casa.", "img//h-rod-spicy.jpeg"))
-productos.push(new Producto("h", "h4", "Rod Champi", 6900, "", "Deliciosa hamburguesa sobre una cama de lechuga y tomate, cubierta con champiñones salteados, cebolla caramelizada y palta.", "img/h-rod-champi.jpeg"))
-productos.push(new Producto("h", "h5", "Rod Guaca Guaca", 6900, "", "Jugosa hamburguesa con cheddar sobre una cama de lechuga, cubierta con pepinillos y el delicioso guacamole de la casa.", "img//h-rod-guacamole.jpeg"))
-productos.push(new Producto("h", "h6", "Rod Big Boss Special", 7500, "", "Jugosa hamburguesa doble con cheddar, tocino, aros de cebolla y un huevo frito más tomate lechuga y cebolla morada.", "img//h-rod-big-boss-special.jpeg"))
-productos.push(new Producto("h", "h7", "Rod Champi-oink Triple", 8500, "", "Tres hamburguesas (si, 3) con cheddar, champiñones salteados, cebolla caramelizada y palta. Solo para valientes.", "img/h-rod-champi-oink-triple.jpeg" ))
-productos.push(new Producto("h", "h8", "Rod La Fresquita", 4990, "", 'Deliciosas croqueta de "poroto negro-quinoa-mani" sobre cama de lechuga y tomate, con pepinillos y cebolla morada.', "img/h-rod-la-fresquita-veggie.jpeg"))
-productos.push(new Producto("s", "a1","Rod Bacon Fries", 3500, "", "Crujientes papas fritas cubiertas con queso cheddar y tocino.", "img/a-rod-bacon-fries.jpeg"))
-productos.push(new Producto("s", "a2", "Rod Guaca Fries", 3700, "", "Crujientes papas fritas cubiertascon un exquisito guacamole.", "img/a-rod-guaca-fries.jpeg"))
-productos.push(new Producto("s", "a3", "Rod Jalapeño Fries", 3700, "", "Crujientes papas fritas cubiertas con cheddar y ají jalapeño.", "img/a-rod-jalapeno-fries.jpeg" ))
-productos.push(new Producto("s", "a4", "Canasta Nugget", 3700, "", "Deliciosos nuggets, empanaditas de queso y papas fritas. Un clásico.", "img/a-canasta-nugget.jpeg" ))
-productos.push(new Producto("b", "b1", "Coca Cola", 1000, "", "Lata 350cc", "img/coca-cola.jpg"))
-productos.push(new Producto("b", "b2", "Fanta", 1000, "", "Lata 350cc", "img/fanta.jpg"))
-productos.push(new Producto("b", "b3", "Sprite", 1000, "", "Lata 350cc", "img/sprite.jpg" ))
-productos.push(new Producto("b", "b4", "Jugo del Valle", 1000, "", "Botella 300cc", "img/jugo-naranja.png" ))
+productos.push(new Producto("h", "11", "Rod Big Boss", 6500, 1,"", "Jugosa hamburguesa doble con cheddar, tocino, aros de cebolla y un huevo frito.", "img/h-rod-big-boss.jpeg"))
+productos.push(new Producto("h", "12", "Rod Dills", 5500, 1, "", "Jugosa hamburguesa sobre cama de lechuga, con cheddar pepinillos y mayonesa casera, simplemente deliciosa.", "img/h-rod-dills.jpeg"))
+productos.push(new Producto("h", "13", "Rod Spicy", 6600, 1, "", "Para los amantes del picante, deliciosa hamburguesa con cheddar, jalapeño, cebolla morada en cuadritos, papas hilo y la salsa secreta de la casa.", "img//h-rod-spicy.jpeg"))
+productos.push(new Producto("h", "14", "Rod Champi", 6900, 1, "", "Deliciosa hamburguesa sobre una cama de lechuga y tomate, cubierta con champiñones salteados, cebolla caramelizada y palta.", "img/h-rod-champi.jpeg"))
+productos.push(new Producto("h", "15", "Rod Guaca Guaca", 6900, 1, "", "Jugosa hamburguesa con cheddar sobre una cama de lechuga, cubierta con pepinillos y el delicioso guacamole de la casa.", "img//h-rod-guacamole.jpeg"))
+productos.push(new Producto("h", "16", "Rod Big Boss Special", 7500, 1, "", "Jugosa hamburguesa doble con cheddar, tocino, aros de cebolla y un huevo frito más tomate lechuga y cebolla morada.", "img//h-rod-big-boss-special.jpeg"))
+productos.push(new Producto("h", "17", "Rod Champi-oink Triple", 8500, 1, "", "Tres hamburguesas (si, 3) con cheddar, champiñones salteados, cebolla caramelizada y palta. Solo para valientes.", "img/h-rod-champi-oink-triple.jpeg" ))
+productos.push(new Producto("h", "18", "Rod La Fresquita", 4990, 1, "", 'Deliciosas croqueta de "poroto negro-quinoa-mani" sobre cama de lechuga y tomate, con pepinillos y cebolla morada.', "img/h-rod-la-fresquita-veggie.jpeg"))
+productos.push(new Producto("s", "21","Rod Bacon Fries", 3500, 1, "", "Crujientes papas fritas cubiertas con queso cheddar y tocino.", "img/a-rod-bacon-fries.jpeg"))
+productos.push(new Producto("s", "22", "Rod Guaca Fries", 3700, 1, "", "Crujientes papas fritas cubiertascon un exquisito guacamole.", "img/a-rod-guaca-fries.jpeg"))
+productos.push(new Producto("s", "23", "Rod Jalapeño Fries", 3700, 1, "", "Crujientes papas fritas cubiertas con cheddar y ají jalapeño.", "img/a-rod-jalapeno-fries.jpeg" ))
+productos.push(new Producto("s", "24", "Canasta Nugget", 3700, 1, "", "Deliciosos nuggets, empanaditas de queso y papas fritas. Un clásico.", "img/a-canasta-nugget.jpeg" ))
+productos.push(new Producto("b", "31", "Coca Cola", 1000, 1, "", "Lata 350cc", "img/coca-cola.jpg"))
+productos.push(new Producto("b", "32", "Fanta", 1000, 1, "", "Lata 350cc", "img/fanta.jpg"))
+productos.push(new Producto("b", "33", "Sprite", 1000, 1, "", "Lata 350cc", "img/sprite.jpg" ))
+productos.push(new Producto("b", "34", "Jugo del Valle", 1000, 1, "", "Botella 300cc", "img/jugo-naranja.png" ))
 
 let listadoCodigos = ["H1" ,"H2", "H3", "H4", "H5", "H6", "H7", "H8", "S1", "S2", "S3", "S4", "B1", "B2", "B3", "B4"]
 
@@ -58,7 +59,12 @@ function Carta(seccion, producto){
             </div>
             <div class="boton-card card-footer">
                 <p>$ ${producto.precio}</p>
-                <a href="#" class="btn btn-warning">Agregar al carro</a>
+                <!-- <div class="input-group">
+                    <button class="btn btn-sm btn-warning" onclick="Aumentar()" name="aumenta">+</button>
+                    <input type="number" value=${producto.cantidad} size="1" class="form-sm lg-2" placeholder="1" aria-label="Cantidad">
+                    <button class="btn btn-sm btn-warning"  onclick="Disminuir()" name="disminuye">-</button>                   
+                </div> -->
+                <button class="btn btn-warning boton"  onclick="AgregarAlCarro(${producto.codigoVenta})" >Agregar al carro</button>  
             </div>
         </div>
     </div>`
@@ -76,6 +82,46 @@ for (let producto of productos){
 }
 
 
+//const btnAumentar = document.querySelector(`.input-group input[type="aumenta"]`)
+const cantidad = document.getElementsByClassName("form-sm")
+
+//const btnAgregar = document.querySelector(".boton")
+const carro = []
 
 
 
+function AgregarAlCarro (codigoVenta) {
+    const productoAgregado = productos.find(producto => producto.codigoVenta == codigoVenta);
+    if (carro.includes(productoAgregado)) {
+        ++ productoAgregado.cantidad 
+    } else {
+        carro.push(productoAgregado)
+        }
+    mostrarCarro(carro)
+}
+function mostrarCarro(prod) {
+    carrito.innerHTML = "";
+    totalMonto = 0
+    totcant = 0
+    prod.forEach(element => {
+        let subt = parseInt(element.cantidad)*parseInt(element.precio)
+        totalMonto += subt
+        let cont = element.cantidad
+        totcant += cont
+        carrito.innerHTML += 
+        `<div><p>${element.cantidad}</p></div>   
+        <div><p>${element.nombre}</p></div>
+        <div><p>${element.precio}</p></div>
+        <div><p>${subt}</p></div>
+        `
+        total.innerText= totalMonto
+        contador.innerText = totcant
+    });
+
+    }
+
+let total = document.getElementById("total")    
+let carrito = document.getElementById("carrito")
+let contador = document.getElementById("contador")
+
+let totcant = 0
